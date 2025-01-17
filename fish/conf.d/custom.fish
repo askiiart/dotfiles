999cba0f41459235ed61519c68c8ed576102cd1a
askiiart <dev@askiiart.net>
2025-01-17 07:27:46 -0600

    set DOCKER_HOST for podman

--- fish/conf.d/custom.fish
+++ fish/conf.d/custom.fish
@@ -4,7 +4,8 @@ if status is-interactive
     set -x GPG_TTY (tty)
     set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     set -x EDITOR nvim
-    #set -x DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
+    # TODO: add logic for podman/docker
+    set DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
     gpgconf --launch gpg-agent
     gpg-connect-agent updatestartuptty /bye
     # ctrl+backspace (^H in kitty)
