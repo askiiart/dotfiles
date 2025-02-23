9856249d3d66d34920e64be0846c07e73f4ff610
askiiart <dev@askiiart.net>
2025-02-23 13:24:32 -0600

    don't require addons to be signed

--- librewolf/prefs.js
+++ librewolf/prefs.js
@@ -8,3 +8,4 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.compactmode.show", true);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("xpinstall.signatures.required", false);
