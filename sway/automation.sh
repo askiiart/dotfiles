3b10116e5bc42a86c6ad09858cdb1ef7eeaf80dc
askiiart <dev@askiiart.net>
2025-02-24 19:45:20 -0600

    add comment documenting idiocy

--- sway/automation.sh
+++ sway/automation.sh
@@ -4,6 +4,9 @@
 
 generate() {
     magick background.png -resize ${1}x${2}! background-${1}x${2}.png
+    # TODO: background.png wasn't the background, it was already overlaid,
+    # so really it was just getting resized, and it just wans't noticeable at lower resolutions?
+    # need to redo it to grow logo.png to match to scale against 1080p (based on whichever axis grows less)
     magick composite -gravity center logo.png background-${1}x${2}.png result-${1}x${2}.png
     cp result-${1}x${2}.png ~/.config/sway/backgrounds/${1}x${2}.png
     rm background-*
