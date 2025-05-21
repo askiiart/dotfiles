98b65efabdcd78a5be62324ab147b8f11b6ec876
askiiart <dev@askiiart.net>
2025-05-21 17:33:14 -0500

    disable selinux gooder

--- distro-specific-scripts/fedora-sway.sh
+++ distro-specific-scripts/fedora-sway.sh
@@ -27,18 +27,5 @@ fi
 # make GUI stuff work when running as root (e.g. gparted)
 xhost +SI:localuser:root
 
-# Install catppuccin grub theme
-#git clone --depth=1 https://github.com/catppuccin/grub
-#sudo mkdir /etc/grub-themes/
-#sudo cp -r ./grub/src/* /etc/grub-themes/
-#rm -rf grub/
-#echo 'GRUB_THEME=/etc/grub-themes/catppuccin-mocha-grub-theme/theme.txt' | sudo tee /etc/default/grub
-#sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# greetd
-#sudo dnf install tuigreet -y
-#sudo cp $GIT_DIR/greetd/config.toml /etc/greetd/config.toml
-#sudo systemctl disable lightdm.service
-#sudo systemctl enable greetd.service
-
-sudo sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+# disable selinux
+sudo sed -i '/^GRUB_CMDLINE_LINUX="/{/selinux=0/!s/"$/ selinux=0"/}' /etc/default/grub
